5c6a77c6f2cc3b1e84593c8698c9d8f475f54bdd
pablozamit <166626152+pablozamit@users.noreply.github.com>
2025-07-23 16:04:59 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -14,7 +15,7 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
-  mode: "development",
+  // La línea 'mode: "development",' ha sido eliminada.
   build: {
     minify: false,
   }
